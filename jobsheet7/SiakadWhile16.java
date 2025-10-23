6371b0dda53c61c1382b335d61ce52f1becbf592
helfa25 <calony64@gmail.com>
2025-10-23 10:41:21 +0700

    Percobaan 2

---
+++ jobsheet7/SiakadWhile16.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class SiakadWhile16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nilai, jml, i = 0; 
+
+        System.out.print("Masukkan jumlah mahasiswa: "); 
+        jml = sc.nextInt(); 
+
+        // Perulangan WHILE (i < jml)
+        while (i < jml) { 
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai = sc.nextInt();
+
+            // Pengecekan validitas nilai
+            if (nilai < 0 || nilai > 100) { 
+                System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
+                continue; // Lompati sisa kode dan ulangi input
+            }
+
+            // Pengecekan Kategori Nilai Huruf
+            if (nilai > 80 && nilai <= 100) { 
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                // Modifikasi (Pertanyaan 4)
+                System.out.println("Bagus, pertahankan nilainya!"); 
+            } else if (nilai > 73 && nilai <= 80) { 
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
+            } else if (nilai > 65 && nilai <= 73) { 
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
+            } else if (nilai > 60 && nilai <= 65) { 
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
+            } else if (nilai > 50 && nilai <= 60) { 
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
+            } else if (nilai > 39 && nilai <= 50) { 
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
+            } else { // E (nilai <= 39)
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
+            }
+            
+            // i++ hanya dieksekusi jika nilai VALID
+            i++; 
+        }
+        
+        System.out.println("\nSemua nilai sudah dikelompokkan.");
+        sc.close();
+    }
+}
