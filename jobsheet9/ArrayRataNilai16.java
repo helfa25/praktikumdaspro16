939ccb6c2fac46e34661068101a1ee6a1ee2e2b2
helfa25 <calony64@gmail.com>
2025-11-04 11:07:43 +0700

    Modifikasi Percobaan 3

--- jobsheet9/ArrayRataNilai16.java
+++ jobsheet9/ArrayRataNilai16.java
@@ -2,24 +2,39 @@ import java.util.Scanner;
 
 public class ArrayRataNilai16 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10]; 
-        double total = 0; 
-        double rata2; 
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jmlMhs = sc.nextInt(); 
 
-        for (int i = 0; i < nilaiMhs.length; i++) { 
-            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ":"); 
-            nilaiMhs[i] = sc.nextInt(); 
-        }
-
-        for (int i = 0; i < nilaiMhs.length; i++) { 
-            total += nilaiMhs[i]; 
-        }
-        rata2 = total / nilaiMhs.length; 
-
-        System.out.println("Rata-rata nilai = " + rata2); 
+        int[] nilaiMhs = new int[jmlMhs];
         
+        double totalLulus = 0;
+        int countLulus = 0;
+        double totalTdkLulus = 0;
+        int countTdkLulus = 0;
+        double rata2Lulus, rata2TdkLulus;
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMhs[i] = sc.nextInt();
+
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                countLulus++;
+            } else {
+                totalTdkLulus += nilaiMhs[i];
+                countTdkLulus++;
+            }
+        }
+        
+        rata2Lulus = (countLulus > 0) ? (totalLulus / countLulus) : 0.0;
+        rata2TdkLulus = (countTdkLulus > 0) ? (totalTdkLulus / countTdkLulus) : 0.0;
+
+        System.out.println("Rata-rata nilai lulus = " + rata2Lulus); // [cite: 168]
+        System.out.println("Rata-rata nilai tidak lulus = " + rata2TdkLulus); // [cite: 169]
+
         sc.close();
     }
 }
