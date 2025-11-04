d8865e206034f26837557e23222784b8da188be3
helfa25 <calony64@gmail.com>
2025-11-04 10:34:32 +0700

    Modifikasi Percobaan 1

--- jobsheet9/ArrayBilangan16.java
+++ jobsheet9/ArrayBilangan16.java
@@ -1,16 +1,11 @@
 public class ArrayBilangan16 {
     public static void main(String[] args) {
+        // Modifikasi Pertanyaan 2: Inisialisasi sekaligus saat deklarasi [cite: 39]
+        int[] bil = {5, 13, -7, 17};
 
-        int[] bil = new int[4]; //
-
-        bil[0] = 5;   
-        bil[1] = 13;  
-        bil[2] = -7;  
-        bil[3] = 17;  
-
-        System.out.println(bil[0]); //
-        System.out.println(bil[1]); //
-        System.out.println(bil[2]); //
-        System.out.println(bil[3]); //
+        // Modifikasi Pertanyaan 3: Menggunakan for-loop yang benar
+        for (int i = 0; i < 4; i++) { // i < 4 adalah kondisi yang benar
+            System.out.println(bil[i]);
+        }
     }
 }
