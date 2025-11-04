b49d77391c77c49bd2eca6f8ee8c25f1b127629c
helfa25 <calony64@gmail.com>
2025-11-04 10:48:03 +0700

    Modifikasi Percobaan 2

--- jobsheet9/ArrayNilai16.java
+++ jobsheet9/ArrayNilai16.java
@@ -1,21 +1,25 @@
-import java.util.Scanner; // 3. Import Scanner [cite: 51]
+import java.util.Scanner;
 
-public class ArrayNilai16 { 
+public class ArrayNilai16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 4. Deklarasi array nilaiAkhir 10 elemen [cite: 52, 53]
         int[] nilaiAkhir = new int[10]; 
 
-        // 5. Perulangan untuk menerima input dan mengisi elemen array 
-        for (int i = 0; i < 10; i++) { 
-            System.out.print("Masukkan nilai akhir ke-" + i + ": "); 
+        for (int i = 0; i < nilaiAkhir.length; i++) { 
+            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i + 1) + ": "); 
             nilaiAkhir[i] = sc.nextInt(); 
         }
 
-        // 6. Perulangan untuk menampilkan semua isi elemen [cite: 58, 59, 60, 61]
-        for (int i = 0; i < 10; i++) { 
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        System.out.println("\n--- Hasil Status Kelulusan ---");
+
+        for (int i = 0; i < nilaiAkhir.length; i++) { 
+
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i + 1) + " LULUS!"); // Menggunakan i+1 agar nomor mahasiswa dimulai dari 1
+            } else {
+                System.out.println("Mahasiswa ke-" + (i + 1) + " TIDAK LULUS!");
+            }
         }
         
         sc.close();
