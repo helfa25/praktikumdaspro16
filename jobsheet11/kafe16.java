1f61108a463094a9165085cc38a111eed4693208
helfa25 <calony64@gmail.com>
2025-12-01 20:15:50 +0700

    modifikasipercobaan2

--- jobsheet11/kafe16.java
+++ jobsheet11/kafe16.java
@@ -1,17 +1,37 @@
 public class kafe16 {
-    public static void Menu() {
-        System.out.println("== MENU RESTO KAFE ==");
-        System.out.println("1. Kopi Hitam - Rp 15,000");
-        System.out.println("2. Cappuccino - Rp 20,000");
-        System.out.println("3. Latte - Rp 22,000");
-        System.out.println("4. Teh Tarik - Rp 12,000");
-        System.out.println("5. Roti Bakar - Rp 10,000");
-        System.out.println("6. Mie Goreng - Rp 18,000");
-        System.out.println("============================");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
+
+     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam   - Rp 15,000");
+        System.out.println("2. Cappuccino   - Rp 20,000");
+        System.out.println("3. Latte        - Rp 22,000");
+        System.out.println("4. Teh Tarik    - Rp 12,000");
+        System.out.println("5. Roti Bakar   - Rp 10,000");
+        System.out.println("6. Mie Goreng   - Rp 18,000");
+        System.out.println("===========================");
+        
+         System.out.println("Status Kode Promo: " + kodePromo);
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Selamat! Anda mendapatkan diskon 50%.");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Selamat! Anda mendapatkan diskon 30%.");
+        } else {
+            System.out.println("Kode promo invalid / tidak berlaku.");
+        }
+
+        System.out.println("Silakan pilih menu yang Anda inginkan.\n");
     }
 
     public static void main(String[] args) {
-        Menu();
+
+        Menu("Andi", true, "DISKON50");
+        
+        System.out.println("(Program Percobaan 2 selesai)");
     }
 }
